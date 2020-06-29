5c18a20c00ab6437ed81466f9298d05192abd578
Vadim <bardiervadim97@gmail.com>
2020-06-29 19:52:53 +0300

    'add_shop_app_with_models_and_basic_views'

---
+++ shop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product, Category
+
+
+# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html',
+                  {'products': products, 'categories': categories, 'category': category})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, available=True, slug=slug)
+    return render(request, 'shop/product/detail.html', {'product': product})
